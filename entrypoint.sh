e0a00707d818fe1b124026a96d87d8c9f65486f9
akablockchain2 <168350013+akablockchain2@users.noreply.github.com>
2024-08-13 16:01:35 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
-# Проверяем, установлена ли переменная окружения API_URL
 if [ -z "$API_URL" ]; then
   echo "API_URL is not set. Exiting."
   exit 1
 fi
 
-# Замена плейсхолдера в конфигурации Nginx
 envsubst '${API_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-# Проверка содержимого сгенерированного файла конфигурации
 echo "Generated /etc/nginx/nginx.conf:"
 cat /etc/nginx/nginx.conf
 
-# Запуск Nginx
 nginx -g 'daemon off;'
